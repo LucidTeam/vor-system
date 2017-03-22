c9d84108d1c3bd22b5a927729b07c2edce43a8a5
Yohan Yang <yohany@hawaii.edu>
2017-03-21 22:01:42 -1000

    vor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,53 @@
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main
+{
 
-      System.out.println("Hello World!");
+    public static void   main(String[] args)
+    {
+
+      Scanner input = new Scanner(System.in);
+
+      double obsDesired;
+      double currentLoc;
+      double diff;
+
+      System.out.println("VHF Omni Directional Radio Range(VOR) System");
+      System.out.print("Enter desired obs: ");
+      obsDesired = input.nextInt();
+      System.out.print("Enter current location: ");
+      currentLoc = input.nextInt();
+
+      diff = getDifference(obsDesired, currentLoc);
+
+      System.out.println("You need to set your course by " + diff + " to get your get your OBS");
+
+
+    }
+
+
+    public static double getDifference(double obs, double currentPos)
+    {
+      if (obs < currentPos)
+      {
+        if (Math.abs(currentPos - obs) > 180)
+
+          return Math.abs((360 - currentPos) + obs);
+
+        else
+
+          return (-1 * Math.abs(obs - currentPos));
+      }
+
+      else
+      {
+        if (Math.abs(obs - currentPos) > 180)
+
+          return Math.abs((360 - obs) + currentPos);
+
+        else
+
+          return (-1 * Math.abs(currentPos - obs));
+      }
     }
 }
